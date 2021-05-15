5c1cd5a94087b8c85ff9213802b6367dc1eb0670
Andrey Kondalov <andrey@kondalov.com>
2021-05-16 01:23:51 +0400

    Updates

---
+++ precache-manifest.5412c8db4d15b62f5c4420f6340d31b5.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff2dc3226af48ab37ff",
+    "url": "0.322acbfe.iframe.bundle.js"
+  },
+  {
+    "revision": "10f6632c9ef6253b78ba",
+    "url": "4.fa31ef7e.iframe.bundle.js"
+  },
+  {
+    "revision": "0e68a261e4846a1e390826c53c553105",
+    "url": "4.fa31ef7e.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5189371baa49412f6ec",
+    "url": "5.947ed842.iframe.bundle.js"
+  },
+  {
+    "revision": "2891fcb226ee789429c1",
+    "url": "6.c08fc28a.iframe.bundle.js"
+  },
+  {
+    "revision": "302c8a8147aa49b2a4f47a49ec36a41c",
+    "url": "6.c08fc28a.iframe.bundle.js.LICENSE.txt"
+  },
+  {
+    "revision": "89c477edc19fdf21a8be",
+    "url": "7.8944eb29.iframe.bundle.js"
+  },
+  {
+    "revision": "33993eb265fc108c204362335d5adba3",
+    "url": "iframe.html"
+  },
+  {
+    "revision": "8b751455cc45ea195c74",
+    "url": "main.502f0b87.iframe.bundle.js"
+  },
+  {
+    "revision": "719cebcada2a6632978c",
+    "url": "runtime~main.c2cadf62.iframe.bundle.js"
+  },
+  {
+    "revision": "e7304494fa9e6b9c3ae2",
+    "url": "vendors~main.8bd9d067.iframe.bundle.js"
+  },
+  {
+    "revision": "25659aa5b361e026cba8b02c899cd6f0",
+    "url": "vendors~main.8bd9d067.iframe.bundle.js.LICENSE.txt"
+  }
+]);
