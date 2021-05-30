cc536aa3c1a5c0b121e77b349ce43830e4afb913
Andrey Kondalov <andrey@kondalov.com>
2021-05-30 19:28:03 +0400

    docs: add argTypes & placeholder to Input stories

--- docs/src/Input.stories.js
+++ docs/src/Input.stories.js
@@ -7,6 +7,16 @@ export default {
   title: 'UI/Input',
   component: Input,
   argTypes: {
+    view: {
+      control: 'select',
+      options: ['primary', 'danger'],
+    },
+    rounded: {
+      control: 'boolean'
+    },
+    type: {
+      control: 'string',
+    }
   },
 };
 
@@ -17,71 +27,87 @@ const Template = (args) => (
   </div>
 );
 
+const commonArgs = {
+  placeholder: 'Введите текст',
+};
 
 export const Rounded = Template.bind({});
 Rounded.args = {
+  ...commonArgs,
   rounded: true,
 };
 
 export const RoundedIcon = Template.bind({});
 RoundedIcon.args = {
+  ...commonArgs,
   rounded: true,
   iconLeft: <CreditCard />
 };
 
 export const RoundedTextarea = Template.bind({});
 RoundedTextarea.args = {
+  ...commonArgs,
   rounded: true,
   type: 'textarea',
 };
 
 export const RoundedTextareaIcon = Template.bind({});
 RoundedTextareaIcon.args = {
+  ...commonArgs,
   rounded: true,
   type: 'textarea',
   iconLeft: <CreditCard />
 };
 
 export const Default = Template.bind({});
+Default.args = commonArgs;
 
 export const IconLeft = Template.bind({});
 IconLeft.args = {
+  ...commonArgs,
   iconLeft: <CreditCard />
 };
 
 export const IconRight = Template.bind({});
 IconRight.args = {
+  ...commonArgs,
   iconRight: <CreditCard />
 };
 
 export const IconBoth = Template.bind({});
 IconBoth.args = {
+  ...commonArgs,
   iconLeft: <CreditCard />,
   iconRight: <CreditCard />
 };
 
 export const Email = Template.bind({});
 Email.args = {
+  ...commonArgs,
   type: 'email',
 };
 
 export const Password = Template.bind({});
 Password.args = {
+  ...commonArgs,
   type: 'password',
 };
 
 export const Textarea = Template.bind({});
 Textarea.args = {
+  ...commonArgs,
   type: 'textarea',
 };
 
 export const TextareaIcon = Template.bind({});
 Textarea.args = {
+  ...commonArgs,
   type: 'textarea',
   iconLeft: <CreditCard />
 };
 
 export const Error = Template.bind({});
 Error.args = {
+  ...commonArgs,
   view: 'danger',
 };
